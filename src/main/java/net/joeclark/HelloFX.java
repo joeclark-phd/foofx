b98f0bdf6d2a366a180f0abaebc2f4601d5fb5dc
joeclark-phd <joe@joeclark.net>
2020-01-17 14:24:49 -0500

    mess around with scenes

--- src/main/java/net/joeclark/HelloFX.java
+++ src/main/java/net/joeclark/HelloFX.java
@@ -1,9 +1,11 @@
 package net.joeclark;
 
 import javafx.application.Application;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -19,37 +21,23 @@ public class HelloFX extends Application {
     public void start(Stage stage) throws Exception {
 
         // things you can do with the stage (i.e., the window)
-        stage.setTitle("Hello World");
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l),640,480);
-        stage.setScene(scene);
+        stage.setTitle("Episode 4 - Stages");
+        stage.setWidth(400);
+        stage.setHeight(500);
+
+        VBox parent = new VBox(); // a layout manager that will be the "root node" of our scene's layout
+
+        // adding child nodes to the layout manager
+        Label label1 = new Label("This is a text label"); // child node
+        parent.getChildren().add(label1);
+
+
+        Scene scene1 = new Scene(parent); // argument is a Parent (usually a layout manager)
+        scene1.setCursor(Cursor.CROSSHAIR); // mouse cursor type can be specific to scene
+        // set the scene to the window
+        stage.setScene(scene1);
+
         stage.show();
-
-
-        // launch a second window
-        Stage stage2 = new Stage();
-        stage2.setTitle("Second window");
-        stage2.setWidth(700);
-        stage2.setHeight(100);
-        stage2.setX(0);
-        stage2.setY(0);
-        stage2.show();
-
-        // modality
-        Stage newWindow = new Stage();
-        //newWindow.initModality(Modality.APPLICATION_MODAL); // other windows can't be used until this one is closed
-        //newWindow.initModality(Modality.NONE); // default behavior
-        newWindow.initModality(Modality.WINDOW_MODAL); // owner/parent window can't be used until this one is closed -- but other windows in our application can
-        newWindow.initOwner(stage2);
-        newWindow.initStyle(StageStyle.UNIFIED); // UTILITY etc. might be useful sometimes
-        newWindow.show();
-
-        stage.toFront();
-        stage.setFullScreen(true);
-
-        // and lots of other Stage methods are available...
 
     }
 
