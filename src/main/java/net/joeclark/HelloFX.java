b30cb1aca346b11261abfa16952241bcb4b651e0
joeclark-phd <joe@joeclark.net>
2020-01-17 13:46:03 -0500

    experiment with Stage methods

--- src/main/java/net/joeclark/HelloFX.java
+++ src/main/java/net/joeclark/HelloFX.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class HelloFX extends Application {
 
@@ -15,6 +17,8 @@ public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        // things you can do with the stage (i.e., the window)
         stage.setTitle("Hello World");
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
@@ -22,6 +26,31 @@ public class HelloFX extends Application {
         Scene scene = new Scene(new StackPane(l),640,480);
         stage.setScene(scene);
         stage.show();
+
+
+        // launch a second window
+        Stage stage2 = new Stage();
+        stage2.setTitle("Second window");
+        stage2.setWidth(700);
+        stage2.setHeight(100);
+        stage2.setX(0);
+        stage2.setY(0);
+        stage2.show();
+
+        // modality
+        Stage newWindow = new Stage();
+        //newWindow.initModality(Modality.APPLICATION_MODAL); // other windows can't be used until this one is closed
+        //newWindow.initModality(Modality.NONE); // default behavior
+        newWindow.initModality(Modality.WINDOW_MODAL); // owner/parent window can't be used until this one is closed -- but other windows in our application can
+        newWindow.initOwner(stage2);
+        newWindow.initStyle(StageStyle.UNIFIED); // UTILITY etc. might be useful sometimes
+        newWindow.show();
+
+        stage.toFront();
+        stage.setFullScreen(true);
+
+        // and lots of other Stage methods are available...
+
     }
 
     @Override
